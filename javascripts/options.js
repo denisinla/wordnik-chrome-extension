f1f9779bdfb9634363b5f026bebcf0c5740e9381
Denis Kabistan <dkabistan@guthy-renker.com>
2015-05-28 17:34:18 -0700

    Update: removed jQuery dependency, updated less, used vanilla for ajax.

--- javascripts/options.js
+++ javascripts/options.js
@@ -1,22 +1,28 @@
-$(document).ready(function(){
-$("#results").bind("click", function() {
-    var apiKey = "YOUR API KEY HERE";
-    var wordQuery = $("#wordQuery").val();
-    var url = "http://api.wordnik.com/v4/word.json/"+ wordQuery +"/definitions?includeRelated=false&includeTags=false&limit=3&useCanonical=false&api_key="+ apiKey;
-    $.getJSON(url, function(data) {
-        var defList = $("<ul />");
+var getWNAPI = (function(){
 
-        $.each(data, function(index, item) {
-            $("<li>"+item.text+"</li>").append("<p class='source'>"+item.attributionText+"</p>")
-                .appendTo(defList);
-                console.log(defList);                
-        });
-        
-        $("#output").fadeOut("fast", function(){
-            $(this).empty()
-                .append(defList)
-                .fadeIn("slow");            
-        });
-    });
-});
-});
+  var apiKey    = "1f94ba32c766af19880780675ba045e9ef6f8f8d37a538e0b";
+  var xhr = new XMLHttpRequest();
+
+  document.getElementById("results").addEventListener("click", function(event){
+    event.preventDefault();
+    // query
+    var wordQuery = document.getElementById("wordQuery").value;
+
+    xhr.open("GET", 'http://api.wordnik.com/v4/word.json/'+ wordQuery +'/definitions?includeRelated=false&includeTags=false&limit=3&useCanonical=false&api_key='+apiKey+'', true);
+    xhr.onreadystatechange = function(){
+
+      if(xhr.readyState == 4 && xhr.status == 200){
+        var data = JSON.parse(xhr.responseText);
+
+        console.log("Data:" + data);
+
+        // results
+        var results = document.getElementById("output");
+        for(var obj in data){
+          results.innerHTML += data[obj].text+ "<br> <span class='source'>"+ data[obj].attributionText + "</span><br><br>";
+        }
+      }
+    }
+    xhr.send(null);
+  });
+})();
